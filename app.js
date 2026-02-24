b0bc6cd6365bc22351e0fdadf4352b79f53a2280
Abhishek <abisheksavalgi326@gmail.com>
2026-02-24 16:01:14 +0530

    seperate route for owners, products and users

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const app = express();
 const cookieParser = require("cookie-parser");
 const path = require('path');
 
+const ownersRouter = require("./routes/ownersRouter");
+const usersRouter = require("./routes/usersRouter");
+const productsRouter = require("./routes/productsRouter");
+
 const db = require("./config/mongoose-connection");
 
 app.use(express.json());
@@ -12,8 +16,8 @@ app.use(express.static(path.join(__dirname,"public")));
 app.set("view engine","ejs");
 app.use(cookieParser());
 
-app.get("/",(req,res)=>{
-    res.send("hey");
-})
+app.use("/owners",ownersRouter);
+app.use("/users",usersRouter);
+app.use("/products",productsRouter);
 
 app.listen(3000);
